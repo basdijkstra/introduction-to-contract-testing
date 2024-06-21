547bcb5a4772389a2b90e1f360f012b31148f918
basdijkstra <bas@ontestautomation.com>
2024-06-21 09:53:57 +0200

    Use new WireMock Pact library

--- order-consumer/src/test/java/order/PaymentServiceClientTest.java
+++ order-consumer/src/test/java/order/PaymentServiceClientTest.java
@@ -1,24 +1,29 @@
 package order;
 
-import com.atlassian.ta.wiremockpactgenerator.WireMockPactGenerator;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 
+import com.github.tomakehurst.wiremock.common.Metadata;
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
 import com.google.gson.Gson;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.client.HttpClientErrorException;
+import se.bjurr.wiremockpact.wiremockpactlib.api.WireMockPactApi;
+import se.bjurr.wiremockpact.wiremockpactlib.api.WireMockPactConfig;
+import se.bjurr.wiremockpact.wiremockpactlib.api.model.WireMockPactMetadata;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Map;
 import java.util.UUID;
 
 @SpringBootTest
@@ -32,16 +37,24 @@ public class PaymentServiceClientTest {
     private static final int AMOUNT = 42;
     private static final String DESCRIPTION = String.format("Payment for order %s", ORDER_ID);
 
-    @Autowired
-    private WireMockServer wireMockServer;
+    private static WireMockServer wireMockServer;
+    private static WireMockPactApi wireMockPactApi;
 
     @BeforeAll
     void configureWiremockPactGenerator() {
 
-        wireMockServer.addMockServiceRequestListener(
-                WireMockPactGenerator
-                        .builder("order_consumer", "payment_provider")
-                        .build());
+        wireMockServer = new WireMockServer();
+
+        wireMockServer.start();
+
+        wireMockPactApi =
+                WireMockPactApi.create(
+                        new WireMockPactConfig()
+                                .setConsumerDefaultValue("order_consumer")
+                                .setProviderDefaultValue("payment_provider")
+                                .setPactJsonFolder("target/pacts"));
+
+        wireMockPactApi.clearAllSaved();
     }
 
     @Test
@@ -53,7 +66,12 @@ public class PaymentServiceClientTest {
 
         wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo(String.format("/payment/%s", ORDER_ID)))
                 .willReturn(aResponse().withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(paymentAsJson)));
+                        .withBody(paymentAsJson))
+                .withMetadata(new Metadata(
+                        Map.of(
+                                WireMockPactMetadata.METADATA_ATTR,
+                                new WireMockPactMetadata()
+                                        .setProvider("payment_provider")))));
 
         Payment responsePayment = new PaymentServiceClient(wireMockServer.baseUrl()).getPaymentForOrder(ORDER_ID.toString());
 
@@ -68,7 +86,12 @@ public class PaymentServiceClientTest {
     public void getPayment_nonexistentOrderId_shouldThrowException() {
 
         wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo("/payment/00000000-0000-0000-0000-000000000000"))
-                .willReturn(aResponse().withStatus(404)));
+                .willReturn(aResponse().withStatus(404))
+                .withMetadata(new Metadata(
+                        Map.of(
+                                WireMockPactMetadata.METADATA_ATTR,
+                                new WireMockPactMetadata()
+                                        .setProvider("payment_provider")))));
 
         assertThatThrownBy(
                 () -> new PaymentServiceClient(wireMockServer.baseUrl()).getPaymentForOrder("00000000-0000-0000-0000-000000000000")
@@ -80,11 +103,27 @@ public class PaymentServiceClientTest {
     public void getPayment_invalidOrderId_shouldThrowException() {
 
         wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo("/payment/this_is_not_a_valid_payment_id"))
-                .willReturn(aResponse().withStatus(400)));
+                .willReturn(aResponse().withStatus(400))
+                .withMetadata(new Metadata(
+                        Map.of(
+                                WireMockPactMetadata.METADATA_ATTR,
+                                new WireMockPactMetadata()
+                                        .setProvider("payment_provider")))));
 
         assertThatThrownBy(
                 () -> new PaymentServiceClient(wireMockServer.baseUrl()).getPaymentForOrder("this_is_not_a_valid_payment_id")
         ).isInstanceOf(HttpClientErrorException.class)
                 .hasMessageContaining("400 Bad Request");
+    }
+
+    @AfterAll
+    public static void after() {
+
+        for (final ServeEvent serveEvent : wireMockServer.getAllServeEvents()) {
+            wireMockPactApi.addServeEvent(serveEvent);
+        }
+
+        wireMockPactApi.saveAll();
+        wireMockServer.stop();
     }
 }
